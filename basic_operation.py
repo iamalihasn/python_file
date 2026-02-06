d19e028fa2f915ae2b6d414f73f07d613c0fc7b3
iamalihasn <alihasancsestudent@gmail.com>
2026-02-06 11:44:06 +0530

    Assignment

---
+++ basic_operation.py
@@ -0,0 +1,16 @@
+# input from user
+number_1 = int(input("Enter first number : "))
+number_2 = int(input("Enter second number : "))
+
+
+# addition
+print(f"Addition = {number_1 + number_2}")
+
+# subtraction
+print(f"Subtraction = {number_1 - number_2}")
+
+# multiplication
+print(f"multiplication = {number_1 * number_2}")
+
+# division
+print(f"division = {number_1 / number_2}")
---
+++ greating.py
@@ -0,0 +1,6 @@
+first_name = input("Enter first name : ")
+last_name = input("Enter last name : ")
+
+full_name = first_name + " " + last_name
+
+print(f"Hello {full_name}! welcome to my world")
